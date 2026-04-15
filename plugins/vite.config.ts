b5f258c49a431d8b61232d420c072dccc6d552fe
Kevin Tun <kevin.tun@northwestern.edu>
2026-04-15 17:48:52 -0500

    feat(plugins): migrate plugin build process to Vite for faster builds
    The build process for the plugins has been migrated to Vite, which offers faster build times and improved development experience. The old rollup build script was removed and replaced with a vite build script. Also, the esmRolldownConfig.ts file was added to configure the rolldown options.

--- plugins/vite.config.ts
+++ plugins/vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import rolldownOptions from "./esmRolldownConfig"
 
 export default defineConfig({
   plugins: [react({ jsxRuntime: "classic" })],
   build: {
     lib: {
       entry: {
-        "hello-world": "hello-world/index.js",
         "gene-info": "gene-info/index.tsx",
       },
       formats: ["es"],
     },
     outDir: "../jbrowse2/plugins",
     emptyOutDir: false,
+    rolldownOptions
   },
 })
