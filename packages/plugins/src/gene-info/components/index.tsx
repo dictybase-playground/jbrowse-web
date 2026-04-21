307c5f558b023702da31f9a9cbe17771410b0ad3
Kevin Tun <kevin.tun@northwestern.edu>
2026-04-21 10:57:01 -0500

    feat(gene-info): change import statement to include React explicitly
    The import statement was changed to include React explicitly, which is required for the component to function correctly.

--- packages/plugins/src/gene-info/components/index.tsx
+++ packages/plugins/src/gene-info/components/index.tsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { match, P } from "ts-pattern"
 
 type Feature = {
   id: string
 }
-
 type GeneInfo = {
   id: string
   name_description: string[]
